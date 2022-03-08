362d96b4ab98c1e95739f0e4d78f9029002fca6a
nickvogel <nick0vogel@gmail.com>
2022-03-08 10:00:03 -0500

    update git

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, email_validator, email
+from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
 ##WTForm
